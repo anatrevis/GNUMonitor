ce09fd054713e56219dc264e18dc7a2ffeae862d
Ana Trevisan <anatrevis@harleenkaur2.scss.tcd.ie>
2019-03-07 18:04:15 +0000

    throughput monitoring

--- agent/reqrep_client.py
+++ agent/reqrep_client.py
@@ -2,10 +2,13 @@ import zmq
 import sys
 import time
 import json
+import xmlrpc.client
+from datetime import datetime
 
-from faker import Faker
+#from faker import Faker
 
-fakegen = Faker()
+#fakegen = Faker()
+client = xmlrpc.client.ServerProxy("http://192.168.56.2:8080")
 
 port = "5556"
 if len(sys.argv) > 1:
@@ -18,12 +21,14 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:%s" % port)
 
 while True:
-    time.sleep (3)
+    time.sleep (5)
     print ("Sending request...")
-    fake_time = str(fakegen.date_time())
-    print (fake_time)
-    fake_throughput = fakegen.random_number()
-    toSendJson = json.dumps({'throughput': fake_throughput, 'datetime': fake_time})
+    timenow = str(datetime.now())
+    #fake_time = str(fakegen.date_time())
+    print (timenow)
+    #fake_throughput = fakegen.random_number()
+    throughput = client.get_user_throughput()
+    toSendJson = json.dumps({'throughput': throughput, 'datetime': timenow})
     print (toSendJson)
     socket.send_json(toSendJson)
     #socket.send_string ("Data: {Througput: " + str(fake_throughput) +", Time: " + str(fake_time) + "}")
