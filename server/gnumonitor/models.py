a4fe6004a0cead37adc3faa82cc1309f6974946c
Ana Trevisan <anatrevis@harleenkaur2.scss.tcd.ie>
2019-03-14 17:41:02 +0000

    v0

--- server/gnumonitor/models.py
+++ server/gnumonitor/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 class MonitorData(models.Model):
     time = models.DateTimeField()
+    #seconds = time.
     throughput = models.FloatField()
 
-    def __str__(self):
-        return str(self.time)
+    # def __str__(self):
+    #
+    #     return str(self.time)
+
+    #def getSeconds():
+    #    return 10
