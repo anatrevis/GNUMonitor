14f49533bbb4e1f7f113f475211eee7b800f9154
Matias Schimuneck <schimuneck.matias@gmail.com>
2019-05-22 15:03:49 +0100

    New model for multiple VMs

--- server/gnumonitor/models.py
+++ server/gnumonitor/models.py
@@ -5,7 +5,7 @@ class Host(models.Model):
     ip = models.CharField(max_length=264)
 
 class Host_Data(models.Model):
-    host_object = models.ForeignKey(Chart, on_delete=models.PROTECT)
+    host_object = models.ForeignKey(Host, on_delete=models.PROTECT)
     time = models.DateTimeField()
     cpu = models.FloatField()
     ram = models.FloatField()
@@ -17,7 +17,7 @@ class Host_Error(models.Model):
     etype = models.CharField(max_length=264)
 
 class Chart(models.Model):
-    host_object = models.ForeignKey(Host, on_delete=models.PROTECT)
+    host_object = models.ForeignKey(Host, on_delete=models.PROTECT, blank=True, null=True)
     xAxis_Name = models.CharField(max_length=264) # time
     yAxis_Name = models.CharField(max_length=264)
     title = models.CharField(max_length=264)
