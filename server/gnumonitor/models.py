96e669e2adcf9e0307cf4128732ecc762e442a44
Matias Schimuneck <schimuneck@cswireless62-186.scss.tcd.ie>
2019-05-22 13:59:33 +0100

    New model

--- server/gnumonitor/models.py
+++ server/gnumonitor/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
+class Host(models.Model):
+    name = models.CharField(max_length=264)
+    ip = models.CharField(max_length=264)
+
+class Host_Data(models.Model):
+    host_object = models.ForeignKey(Chart, on_delete=models.PROTECT)
+    time = models.DateTimeField()
+    cpu = models.FloatField()
+    ram = models.FloatField()
+
+class Host_Error(models.Model):
+    host_object = models.ForeignKey(Host, on_delete=models.PROTECT)
+    time = models.DateTimeField()
+    description = models.CharField(max_length=264)
+    etype = models.CharField(max_length=264)
 
 class Chart(models.Model):
+    host_object = models.ForeignKey(Host, on_delete=models.PROTECT)
     xAxis_Name = models.CharField(max_length=264) # time
     yAxis_Name = models.CharField(max_length=264)
     title = models.CharField(max_length=264)
